a40cdc8cdf55c7ca781895ff99e0c9baa08b28d5
Lori Dunford <loridk@gmail.com>
2024-05-03 00:47:11 -0400

    Pagination is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
-
+import {NgxPaginationModule} from 'ngx-pagination';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,TableComponent,RouterModule],
+  imports: [HomeComponent,TableComponent,RouterModule,NgxPaginationModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { ENTRIES } from '../../table';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [NgxPaginationModule,CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
   entries = ENTRIES;
+  p: number = 1;
+  collection: any[] = ENTRIES;  
 }
 
  
